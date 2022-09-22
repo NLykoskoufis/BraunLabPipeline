781517be2052ed3063cbd0c82dc1dc3b2f8070e1
NLykoskoufis <nikolaslykos94@gmail.com>
2022-09-22 09:21:55 +0200

    modified peakCounts so that quantification options are not hardcoded

--- src/pipeline_tools/submitSteps.py
+++ src/pipeline_tools/submitSteps.py
@@ -581,7 +581,7 @@ def submitPeak2Counts(configFileDict,NARROWPEAK_FILES,BAM_FILES, dryRun=False):
         #    peak_cmd.append("{featurecounts} -T 4 -O -p -a {peakGTF} -o {outputFile} {inputFile} -t exon -g gene_id".format(featurecounts = configFileDict['featureCounts'], peakGTF = GTF_FILE, outputFile = outputFile, inputFile = bamFile))
         #else:
         #    peak_cmd.append("{featurecounts} -T 4 -O -a {peakGTF} -o {outputFile} {inputFile} -t exon -g gene_id".format(featurecounts = configFileDict['featureCounts'], peakGTF = GTF_FILE, outputFile = outputFile, inputFile = bamFile))
-        peak_cmd.append("{featurecounts} {quantOptions} -a {peakGTF} -o {outputFile} {inputFile} -t exon -g gene_id".format(featurecounts = configFileDict['featureCounts'], quantOptions = configFileDict['quantOptions'] peakGTF = GTF_FILE, outputFile = outputFile, inputFile = bamFile))
+        peak_cmd.append("{featurecounts} {quantOptions} -a {peakGTF} -o {outputFile} {inputFile} -t exon -g gene_id".format(featurecounts = configFileDict['featureCounts'], quantOptions = configFileDict['quantOptions'], peakGTF = GTF_FILE, outputFile = outputFile, inputFile = bamFile))
     
     
     
